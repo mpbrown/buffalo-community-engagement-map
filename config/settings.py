2f7841ef0c6d11b834d5e966d1d41078a5631778
Mike Brown <mpbrown@buffalo.edu>
2020-10-05 15:48:22 -0400

    Update settings with static root

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 BOOTSTRAP4 = {
     # Set placeholder attributes to label if no placeholder is provided
